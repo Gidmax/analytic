2a65f639ea4409af8a6f98d690bdf64a0e6dbbdf
Chinnatip Taemkaeo <chinnatip@me.com>
2017-07-30 02:28:17 +0700

    integrate map#2

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :postcards
 
   get 'home/test'
+  get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root 'home#test'
 end
