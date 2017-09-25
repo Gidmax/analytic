81c00a89d1cb7dc670f249b76f7735485c94fec4
Chinnatip Taemkaeo <chinnatip@me.com>
2017-09-25 20:22:45 +0700

    force ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # force SSL for every redirect https
+  config.force_ssl = true
 end
